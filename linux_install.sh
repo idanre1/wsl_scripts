4136d7c0ec14dade8a117ae7c47dd0e901bdcebf
Idan Regev <idanre1@gmx.com>
2020-09-22 11:59:31 +0300

    Install and python wrappers

---
+++ linux_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#python
+sudo apt install python3 python-is-python3 python3-pip
+
+# jupyter
+sudo apt install jupyter-notebook
+
+# scikits
+pip3 install pandas sklearn
+
+
---
+++ py3_notebook.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export PYTHON_PATH=/home/$USER/wsl_lib
+
+jupyter-notebook $*
+
