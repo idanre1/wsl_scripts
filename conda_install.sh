ff371effe796cb86b2f1986881fda86abcf70dc0
Idan Regev <idanre1@gmx.com>
2020-12-17 17:11:37 +0200

    conda

--- conda_install.sh
+++ conda_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd ~
-wget https://repo.anaconda.com/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh
-chmod +x Miniconda3-4.5.4-Linux-x86_64.sh
-./Miniconda3-4.5.4-Linux-x86_64.sh
+CONDA_FILE=miniconda.sh
+#wget https://repo.anaconda.com/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh $CONDA_FILE
+wget -O $CONDA_FILE https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x $CONDA_FILE
+./$CONDA_FILE -b
 
--- conda_mlfinlab_install.sh
+++ conda_mlfinlab_install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-CONDA_ENV=env_zipline
-conda create -n $CONDA_ENV python=3.6 -y
+CONDA_ENV=env_mlfinlab
+conda create -n $CONDA_ENV python=3.7 -y
 
 #https://stackoverflow.com/questions/55507519/python-activate-conda-env-through-shell-script/58081608#58081608
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate $CONDA_ENV
 
-conda install -c conda-forge zipline -y
+conda install -c conda-forge mlfinlab -y
 
 # /home/idanre1/miniconda3/envs/env_zipline
 # To activate this environment, use:
@@ -18,6 +18,6 @@ conda install -c conda-forge zipline -y
 
 
 #https://0xboz.github.io/blog/how-to-install-zipline-debian-stretch/
-conda install -c conda-forge jupyterlab -y
-conda install -c conda-forge matplotlib -y
-conda install -c Quantopian ta-lib -y
+# conda install -c conda-forge jupyterlab -y
+# conda install -c conda-forge matplotlib -y
+#conda install -c Quantopian ta-lib
